f8b3ca74fe136a596e4dc3c6e3fc15f5c6c9a7f2
Juhana Soini <juhana.soini@avenis.fi>
2020-01-17 15:12:40 +0200

    Commit

--- app.js
+++ app.js
@@ -31,22 +31,45 @@ const server = http.createServer( (req, res ) => {
 	{
 		let form = `
 			<form action="add-note" method="POST" accept-charset="UTF-8">
-				<input type="text" name="note" value="###NOTE###">
+				<input type="text" name="note" value="###NOTE###" tabindex="0">
 				<button type="submit">Add</button>
 			</form>
+			<form action="delete-note" method="POST" accept-charset="UTF-8">
+				<input type="number" name="index" min="0" max="`+(notes.length-1)+`">
+				<button type="submit">Delete</button>
+			</form>
 			<h3>Notet:</h3>
+			<form action="delete-note" method="POST">
 			<ul>
 			###NOTES###
 			</ul>
+			</form>
 		`;
 		let temp = '';
 		notes.map((item, index) => {
-			temp += '<li>'+item+'</li>';
+			temp += `<li>${index}: ${item} <button type="submit" value="${index}" name="index">Delete</button></li>`;
 		});
 		form = form.replace( '###NOTES###', temp );
 		form = form.replace( '###NOTE###', '' );
 		res.write( FRAME.replace( '###CONTENT###', form ) );
 		res.end();
+	}
+	else if( URL == '/delete-note' && METHOD == 'POST' )
+	{
+		let body = [];
+		req.on('data', (chunk) => {
+		  body.push(chunk);
+		}).on('end', () => {
+		  body = Buffer.concat(body).toString();
+		  body = body.split( '=' );
+		  let index = body[1];
+		  notes.splice(index, 1);
+		  res.statusCode = 303;
+		  res.statusMessage = 'Redirect';
+		  res.setHeader( 'Location', '/' );
+		  res.end();
+		});
+
 	}
 	else if( URL == '/add-note' && METHOD == 'POST' )
 	{
@@ -57,7 +80,6 @@ const server = http.createServer( (req, res ) => {
 		  body = Buffer.concat(body).toString();
 		  body = body.split( '=' );
 		  notes.push( body[1] );
-		  console.log( body );
 		  res.statusCode = 303;
 		  res.statusMessage = 'Redirect';
 		  res.setHeader( 'Location', '/' );
